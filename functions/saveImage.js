7ecf70208809f0854b929db2ce3f8c92812b6e2c
Pierre Lingat <pierre@helloworld.college>
2021-01-07 22:25:07 +1100

    Change

--- functions/saveImage.js
+++ functions/saveImage.js
@@ -5,8 +5,7 @@ const html2pdf = require('./helpers/html2pdf');
 
 exports.handler = async (event) => {
     switch(event.httpMethod) {
-        case 'GET':
-            return await html2pdf();
+    
         default:
             return formattedReturn(405, {});
     }
